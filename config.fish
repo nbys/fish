54c7b6d6bbc41ea9581e180474b7c2cb3911556c
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2024-06-03 09:23:03 +0200

    aws autocomplete

--- config.fish
+++ config.fish
@@ -5,3 +5,7 @@ end
 set fish_greeting
 
 pyenv init - | source
+
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
