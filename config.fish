cab9d7d649e8058f9d280cf35d4ffba6850e76ee
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2025-01-05 19:55:41 +0100

    alias for k8s

--- config.fish
+++ config.fish
@@ -9,3 +9,4 @@ pyenv init - | source
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
+alias k kubectl
